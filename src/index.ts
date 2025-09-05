05504d474292c541f517efc51787446d2aceb77f
Bufo <bufo24@users.noreply.github.com>
2025-09-05 17:35:29 +0200

    feat: macaroon permission helpers

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 const { base64ToBytes, importMacaroon } = require('macaroon');
+import { difference, isEqual } from 'lodash';
 // @ts-expect-error lib does not support TS
 import { MacaroonId } from './proto/id_pb';
 
 type MacaroonPermission = { entity: string; actions: string[] };
+type FlatMacaroonPermission = `${string}:${string}`;
 
 export function getMacaroonOperations (
   macaroonHex: string,
@@ -22,4 +24,53 @@ export function getMacaroonOperations (
     entity: op.getEntity(),
     actions: op.getActionsList(),
   }));
+};
+
+
+/**
+ * @returns `(entity:action)[]`
+ */
+export const getFlatPermissionList = (
+  macaroonPermissions: MacaroonPermission[],
+): FlatMacaroonPermission[] => {
+  return macaroonPermissions.reduce((list, p) => {
+    for (const action of p.actions) {
+      list.push(`${p.entity}:${action}`);
+    }
+
+    return list;
+  }, [] as FlatMacaroonPermission[]);
+};
+
+/**
+ * @param givenPermissions Expects format `(entity:action)[]`
+ * @param wantedPermissions Expects format `(entity:action)[]`
+ * @returns permissions of the macaroon
+ */
+export const verifyMacaroonPermissions = (
+  givenPermissions: FlatMacaroonPermission[],
+  wantedPermissions: FlatMacaroonPermission[],
+): string[] => {
+  const matches = isEqual(givenPermissions, wantedPermissions);
+  if (matches) return givenPermissions;
+
+  // Check which permissions are missing
+  const missingPermissions = difference(wantedPermissions, givenPermissions);
+  if (missingPermissions.length) {
+    throw new Error(
+      `Macaroon is missing permissions: ${missingPermissions.join(', ')}`,
+    );
+  }
+
+  // Check which permissions are not wanted
+  for (const permission of givenPermissions) {
+    const need = wantedPermissions.includes(permission);
+    if (!need) throw new Error(`Unwanted permission: ${permission}`);
+  }
+
+  console.error(`Unexpected issue during macaroon permissions check`, {
+    givenPermissions,
+    wantedPermissions,
+  });
+  throw new Error(`Unable to verify macaroon`);
 };
